47547b925d75d093c1b30868719ed951298f54b3
mdswaley <mdswaley075@gmail.com>
2026-07-02 13:21:06 +0530

    Creating auth-service for security purpose

---
+++ src/main/java/com/example/Telecom/Service/Provisioning/Platform/TelecomServiceProvisioningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Telecom.Service.Provisioning.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomServiceProvisioningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomServiceProvisioningPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Telecom/Service/Provisioning/Platform/TelecomServiceProvisioningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Telecom.Service.Provisioning.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomServiceProvisioningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
