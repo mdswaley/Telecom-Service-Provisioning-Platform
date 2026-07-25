fdc5d2a267d942a7bb5c62bce3d58f8a598ccb68
mdswaley <mdswaley075@gmail.com>
2026-07-25 12:02:21 +0530

    Without swagger config the uri sent request to the host path like (jarvix). After configure it is sent to localhost:8080

---
+++ auth-service/src/main/java/com/example/auth_service/Config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.auth_service.Config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080/auth")
+                ))
+                .info(new Info()
+                        .title("Auth Service API")
+                        .version("1.0"));
+    }
+}
