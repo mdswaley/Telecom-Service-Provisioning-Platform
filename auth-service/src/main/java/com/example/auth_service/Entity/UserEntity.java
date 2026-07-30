6b1aa9f5b8e4f8d169b3d68bae0316b70af105dc
mdswaley <mdswaley075@gmail.com>
2026-07-30 11:35:51 +0530

    delete unused imports

--- auth-service/src/main/java/com/example/auth_service/Entity/UserEntity.java
+++ auth-service/src/main/java/com/example/auth_service/Entity/UserEntity.java
@@ -3,7 +3,6 @@ package com.example.auth_service.Entity;
 import com.example.auth_service.Enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.stereotype.Service;
 
 @Entity
 @Setter
