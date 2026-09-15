58cf4e76569b76b59f4293985af41eb0d72fb793
mdswaley <mdswaley075@gmail.com>
2026-09-15 20:26:38 +0530

    Explain why use redis

--- api-gateway/src/main/java/com/example/api_gateway/Service/RedisTokenService.java
+++ api-gateway/src/main/java/com/example/api_gateway/Service/RedisTokenService.java
@@ -20,3 +20,24 @@ public class RedisTokenService {
         return redisTemplate.opsForValue().get("access:" + userId);
     }
 }
+
+//              USER 1
+//                │
+//        ┌───────┴────────┐
+//        ↓                ↓
+//      Login #1          Login #2
+//        ↓                ↓
+//      Token A             Token B
+//      jti=AAA             jti=BBB
+//        │                │
+//        └───────┬────────┘
+//                ↓
+//              Redis
+//          access:1 = BBB
+//                ↑
+//          only latest JTI
+
+// JTI :- means JWT ID
+
+//Instead of storing/reusing the access token, we store the latest access token's jti in Redis and use it
+// to validate whether the incoming token is still the current one.
